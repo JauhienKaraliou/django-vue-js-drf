66d17dd9a0a8710a94fa0cc871671e38fddb36d3
jauhien <jauhien.karaliou@gmail.com>
2020-05-07 11:45:13 +0300

    Settings added

--- vp_app/vp_app/settings.py
+++ vp_app/vp_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3*5&0)#=cy8c5pwk0-6z277@+isbt20^jzo@je&k9a=p!asjr-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.239', 'localhost']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'vpsite',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,10 @@ WSGI_APPLICATION = 'vp_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/etc/mysql/my.cnf',
+        },
     }
 }
 
